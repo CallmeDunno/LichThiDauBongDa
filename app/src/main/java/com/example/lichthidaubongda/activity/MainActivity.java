508ecbeff705c49332329619fb594b1063235c6d
Dunno <dungworkit@gmail.com>
2023-05-16 21:27:58 +0700

    Version 2.0

--- app/src/main/java/com/example/lichthidaubongda/activity/MainActivity.java
+++ app/src/main/java/com/example/lichthidaubongda/activity/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
+
+    /** Issue
+     * - Khó có thể nâng cấp (xem lịch thi đấu các giải đấu khác) do gặp vấn đề về API, API trong project này được lấy của VnExpress
+     * nên khó control được dữ liệu trả về
+     * - Nhiều lịch thi đấu có cùng 1 thời gian => chưa biết cách dùng service để giải quyết vấn đề này.
+     * */
 }
